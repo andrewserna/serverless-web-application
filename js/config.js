42b73399e2dfaff7bccc835d99b9ca63c740d0b3
andrewserna <114114431+andrewserna@users.noreply.github.com>
2023-04-23 22:34:22 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
-        invokeUrl:          'eyJraWQiOiJ4ZmllQ2NWRk9FckxVNjNYbCszYTd6RDBPM1FPbVBKT1dhV3RndVBpY3VnPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIzNmViOTkxNi0xODE5LTRiODUtOGJlMC1iODkwYWY3OTMzOGQiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMi5hbWF6b25hd3MuY29tXC91cy1lYXN0LTJfYlBMRGRUeGdBIiwiY29nbml0bzp1c2VybmFtZSI6ImFzZXJuYTIwMDAtYXQtZ21haWwuY29tIiwib3JpZ2luX2p0aSI6ImUyZmJhZTI1LWJhZGUtNDZhMy1iNmY5LTFjNTE5Njk1NjUzYiIsImF1ZCI6IjMxNWsydWY2bjZtMmJxMmJsdmN1cmZybjQiLCJldmVudF9pZCI6IjM4OTk3N2M1LTc5NDYtNGUxMS1iNjIwLTY3ZDhjZjVmNjc1YyIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjgyMzA1NTc2LCJleHAiOjE2ODIzMDkxNzYsImlhdCI6MTY4MjMwNTU3NiwianRpIjoiNzA0ODM2MTctYTk0Ni00N2QyLWEzOGQtNmFhNjgxMmQ2MmVhIiwiZW1haWwiOiJhc2VybmEyMDAwQGdtYWlsLmNvbSJ9.BgrFfafqa-LcL_tuP52JQCKi_6GZkL9VpbSoJOSa_LQ7phLb9i_p-xb80swdopZA3EwG2P-wnC-YfPecLDKcuNIgl-8bTV1TMqp8MPTc8N06PU6Ie7EncOvobL_QJcVUkIjhuGg3vkXXiz4Rjst6rpr8N6s6-IdYZULONbYhcQIVieVgc87KuFprINmyWNLukjJfdY1jedKKwQeKnJzzBbAbJG-K4Tlu6KG-2FrNBgZENlx2f-gWedR0zqH2YSbfoKoM4KMOG4aCCDryFuxOd86w9FhmhzIxcfJoxNEz41HKZzLbmIY6zlJZeDt-N89kMtAlwoC2bLkYzB779FOyWg'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          'https://2472r6wo92.execute-api.us-east-2.amazonaws.com/p'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
